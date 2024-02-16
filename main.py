2a383af12df9ec8831c796a81a031a26b6cfdeee
operador <anderson.valenzuela@campusucc.edu>
2024-02-15 19:55:35 -0500

    sal

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 class Usuario : 
+
+    contador =  0
+    color ="azul"
     def __init__(self , edad , nombre , cedula):
         self.edad = edad
         self.nombre = nombre
         self.cedula = cedula
         self.tipo = "clientes"
+        Usuario.contador +=1
         print("objeto creado")
     def __str__(self):
-        return f"Hola {self.nombre} su edad es {self.edad}.  Hasta luego!"
+        return f"""
+        Hola {self.nombre} su edad es {self.edad}.  Hasta luego!"
 
-usuario = Usuario(18 , "anderson" , 112233)
+    
+    
+           """
+ 
+usuario = Usuario(18 , "user1" , 112233)
+usuario = Usuario(18 , "user2" , 112233)
+usuario = Usuario(18 , "user3" , 112233)
+   
+print(f"cantidad de obejetos creados {Usuario.contador} ")
 print(usuario)
 print (usuario.tipo)
