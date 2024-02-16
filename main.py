5c6dc39676e076f3311c20c2b516291c2ca41fd2
ifconfig68 <159632338+ifconfig68@users.noreply.github.com>
2024-02-16 00:10:14 +0000

    saludos

---
+++ main.py
@@ -0,0 +1,12 @@
+class Usuario : 
+    def __init__(self):
+        self.edad = 20
+        self.nombre = "Anderson"
+        self.cedula = 1096255949
+        print("objeto creado")
+
+usuario = Usuario()
+print(usuario.nombre)
+usuario1  = Usuario
+print("su edad es : " , usuario.edad)
+    
